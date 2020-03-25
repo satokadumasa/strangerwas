3c31f7980795f527c5e5f3b7f7f41f436b7ff85e
佐藤憲匡 <sato.kadumasa@gmail.com>
2020-03-25 19:27:55 +0900

    composer.json 修正

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-#$autoloader = require "vendor/autoload.php";
-#echo json_encode($autoloader->getPrefixesPsr4(), JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
-$autoloader = require "vendor/autoload.php";
-$result = $autoloader->findFile('strangerfw\core\Route');
-echo json_encode($result);
-exit;
-
